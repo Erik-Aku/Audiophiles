acce8f7b138dc60b9d6ace9e0a231aaca87ade12
ylf456 <145236890+ylf456@users.noreply.github.com>
2024-01-21 16:22:51 -0800

    remove test files

--- Controllers/test1/index.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-const userRoutes = require("./userRoute");
-//const friendTagRoutes = require("");
-//const musicRoutes = require("");
-//const musicTagRoutes = require("");
-
-router.use("/user", userRoutes);
-
-module.exports = router;
