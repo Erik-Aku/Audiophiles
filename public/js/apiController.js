e328d40c81ad38005f001559a6bed0ea9570b3f1
steviesummers <steviesummerss@gmail.com>
2024-01-25 21:32:08 -0800

    fixed bracnh

---
+++ public/js/apiController.js
@@ -0,0 +1,38 @@
+const axios = require('axios');
+//const server = require('server.js');
+
+const rapidApiKey = 'd234ba8234msh21b763e119aeb38p18fabdjsnd2b69705b260';
+const rapidApiHost = 'deezerdevs-deezer.p.rapidapi.com';
+
+async function searchDeezer(query) {
+  try {
+    const searchOptions = {
+        method: 'GET',
+        url: 'https://deezerdevs-deezer.p.rapidapi.com/search',
+        params: { q: query },
+        headers: {
+          'X-RapidAPI-Key': rapidApiKey,
+          'X-RapidAPI-Host': rapidApiHost,
+        }
+      };
+      fetch(searchOptions)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+
+      })
+      .then(data => {
+        searchOptions.textContent = JSON.stringify(data, null, 2);
+      })
+
+  } catch (error) {
+    console.error(error);
+    return ();
+  }
+}
+
+module.exports = {
+  searchDeezer
+};
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers/index.js');
 const helpers = require('./utils/helpers');
-
+const apiController = require('./Public/js/apiController.js');
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
