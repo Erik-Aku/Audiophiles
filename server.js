f8118101dfacccb7488b37bedbfd50f3a13f7d1c
steviesummers <steviesummerss@gmail.com>
2024-01-25 18:50:05 -0800

    fixed api

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const helpers = require('./utils/helpers');
-
+const apiController = require('./Controllers/api/apiController.js');
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
