7229a1dcea16df05a3e11a0967a8f06c05729ddb
Erik  Enoka Aku <Erik@Jennifers-iMac.lan>
2024-01-27 17:22:33 -0800

    Group Effort to get Music Data Displayed on home page

--- public/js/apiController.js
+++ public/js/apiController.js
@@ -1,8 +1,8 @@
 
-const querySubmit = document.querySelector('#querySubmitButton')
-const queryInput = document.querySelector('#queryInputField')
-let queryText
-let url
+let querySubmit = document.querySelector('#querySubmitButton');
+const queryInput = document.querySelector('#queryInputField');
+let queryText;
+let url;
 
 queryInput.addEventListener('input', function() {
   queryText = queryInput.value
@@ -23,9 +23,55 @@ async function fetchData() {
     const result = await response.json();
     // console.log(result.data.map((song) => console.log(song.title)))
     console.log(result.data)
+    songData(result.data);
   } catch (error) {
     console.error(error);
   }
 }
+
+const cardContainerEl = document.querySelector('#music-container');
+
+const songData = (data) => {
+  for (let i = 0; i < data.length; i++) {
+    let currentObj = data[i];
+    let songName = currentObj.title;
+    let artistName = currentObj.artist.name;
+    let albumCover = currentObj.album.cover_medium;
+    
+    
+    const artistCard = document.createElement('div');
+    artistCard.classList.add("card")
+    artistCard.setAttribute('style', "width: 18rem;")
+
+    const cardBody = document.createElement('div');
+    cardBody.classList.add('card-body');
+
+    const cardTitle = document.createElement('h5');
+    cardTitle.classList.add('card-title');
+    cardTitle.textContent = artistName;
+
+    const cardImage = document.createElement('img');
+    cardImage.classList.add("card-img-top");
+    cardImage.setAttribute('src', albumCover,);
+
+   const song = document.createElement('p');
+   song.classList.add('card-text');
+   song.textContent = songName;
+
+   const cardLikeBtn = document.createElement('a');
+   cardLikeBtn.setAttribute('href', "#");
+   cardLikeBtn.textContent = "Like";
+
+    cardBody.appendChild(song)
+    cardBody.appendChild(cardTitle);
+    cardBody.appendChild(song)
+    cardBody.appendChild(cardImage);
+    cardBody.appendChild(cardLikeBtn);
+    artistCard.appendChild(cardBody);
+    cardContainerEl.appendChild(artistCard)
+  }
+
+}
+
 
 querySubmit.addEventListener('click', () => fetchData())
